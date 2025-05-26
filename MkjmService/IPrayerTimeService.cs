0af134c6b2fd3160df2a6da78cbef9ffeaeb454b
ahoque29 <a.hoque29@hotmail.com>
2025-05-26 19:38:18 +0100

    Add api call service

---
+++ MkjmService/IPrayerTimeService.cs
@@ -0,0 +1,8 @@
+﻿using MkjmCommon.ApiModels;
+
+namespace MkjmService;
+
+public interface IPrayerTimeService
+{
+    Task<PrayerTimeResponse?>? GetPrayerTimesAsync(PrayerTimeQuery query);
+}
---
+++ MkjmService/PrayerTimeService.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using Microsoft.AspNetCore.WebUtilities;
+using MkjmCommon.ApiModels;
+
+namespace MkjmService;
+
+public class PrayerTimeService : IPrayerTimeService
+{
+    private readonly HttpClient _httpClient = new();
+
+    public async Task<PrayerTimeResponse?> GetPrayerTimesAsync(PrayerTimeQuery query)
+    {
+        var baseUri = new Uri("https://moonsighting.ahmedbukhasim.sa/time_json.php");
+
+        var queryparams = new Dictionary<string, string?>()
+        {
+            ["year"] = query.Year,
+            ["tz"] = query.Timezone,
+            ["lat"] = query.Latitude.ToString(CultureInfo.InvariantCulture),
+            ["lon"] = query.Longitude.ToString(CultureInfo.InvariantCulture),
+            ["method"] = query.Method.ToString(),
+            ["both"] = query.Both ? "1" : "0",
+            ["time"] = query.Time ? "1" : "0"
+        };
+
+        var queryUrl = QueryHelpers.AddQueryString(baseUri.ToString(), queryparams);
+
+        try
+        {
+            var response = await _httpClient.GetAsync(queryUrl);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var result = JsonSerializer.Deserialize<PrayerTimeResponse>(content, options);
+            return result;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
