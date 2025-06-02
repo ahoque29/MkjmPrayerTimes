c0ee838ce431583c4f43904bcbc59dd3992afd08
ahoque29 <a.hoque29@hotmail.com>
2025-06-02 17:34:35 +0100

    Add year and month selection

--- mkjmwebsite/src/api/usePrayerTimes.ts
+++ mkjmwebsite/src/api/usePrayerTimes.ts
@@ -1,11 +1,12 @@
 ﻿import { useEffect, useState } from "react";
 import type { PrayerTimeResponse } from "../types/prayerTimeTypes.ts";
 
-export function usePrayerTimes(): PrayerTimeResponse | string {
+export function usePrayerTimes(year: number, month: number): PrayerTimeResponse | string {
     const [data, setData] = useState<PrayerTimeResponse | string>("Loading...");
 
     useEffect(() => {
-        fetch("api/PrayerTime/mkjm?year=2026&month=1")
+        setData("Loading...");
+        fetch(`api/PrayerTime/mkjm?year=${year}&month=${month}`)
             .then(res => {
                 if (!res.ok) {
                     throw new Error("Failed to fetch");
@@ -14,7 +15,7 @@ export function usePrayerTimes(): PrayerTimeResponse | string {
             })
             .then(json => setData(json))
             .catch(err => setData("Error: " + err.message));
-    }, []);
+    }, [year, month]);
 
     return data;
 }
