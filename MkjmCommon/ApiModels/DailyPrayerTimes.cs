01d543669b387115194907d63584efa28ba6c660
ahoque29 <a.hoque29@hotmail.com>
2025-06-02 17:48:39 +0100

    Fix handling leap years

--- MkjmCommon/ApiModels/DailyPrayerTimes.cs
+++ MkjmCommon/ApiModels/DailyPrayerTimes.cs
@@ -8,5 +8,21 @@ public class DailyPrayerTimes
     public string Day { get; set; }
     public PrayerTimes Times { get; set; }
 
-    [JsonIgnore] public DateTime Date => Day == null ? DateTime.MaxValue : DateTime.ParseExact(Day[..6], "MMM dd", CultureInfo.InvariantCulture);
+    public DateTime GetDate(int year)
+    {
+        if (string.IsNullOrEmpty(Day) || Day.Length < 6)
+        {
+            return DateTime.MaxValue; // Invalid date format
+        }
+        
+        var monthString = Day[..6];
+        var dateString = $"{monthString} {year}";
+
+        if (!DateTime.TryParseExact(dateString, "MMM dd yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+        {
+            return DateTime.MaxValue; // Invalid date format
+        }
+
+        return date;
+    }
 }
