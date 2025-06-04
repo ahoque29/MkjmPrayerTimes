2081bb6892b81d6fda135ce96a042a7da858acb5
ahoque29 <a.hoque29@hotmail.com>
2025-06-04 19:35:12 +0100

    Add iqamah to prayer table

--- mkjmwebsite/src/components/PrayerTimesTable.tsx
+++ mkjmwebsite/src/components/PrayerTimesTable.tsx
@@ -12,11 +12,16 @@ export function PrayerTimesTable({ data }: PrayerTimesTableProps) {
                 <tr>
                     <th style={thStyle}>Day</th>
                     <th style={thStyle}>Fajr</th>
-                    <th style={thStyle}>Sunrise</th>
+                    <th style={thStyle}>Iqamah</th>
+                    <th style={thStyle}>Shourooq</th>
                     <th style={thStyle}>Dhuhr</th>
+                    <th style={thStyle}>Iqamah</th>
                     <th style={thStyle}>Asr</th>
+                    <th style={thStyle}>Iqamah</th>
                     <th style={thStyle}>Maghrib</th>
+                    <th style={thStyle}>Iqamah</th>
                     <th style={thStyle}>Isha</th>
+                    <th style={thStyle}>Iqamah</th>
                 </tr>
             </thead>
             <tbody>
@@ -24,28 +29,33 @@ export function PrayerTimesTable({ data }: PrayerTimesTableProps) {
                     <tr key={i} style={i % 2 === 0 ? rowStyleEven : rowStyleOdd}>
                         <td style={tdStyle}>{day}</td>
                         <td style={tdStyle}>{times.fajr}</td>
+                        <td style={tdStyle}></td>
                         <td style={tdStyle}>{times.sunrise}</td>
                         <td style={tdStyle}>{times.dhuhr}</td>
+                        <td style={tdStyle}></td>
                         <td style={tdStyle}>{times.asr}</td>
+                        <td style={tdStyle}></td>
                         <td style={tdStyle}>{times.maghrib}</td>
+                        <td style={tdStyle}></td>
                         <td style={tdStyle}>{times.isha}</td>
+                        <td style={tdStyle}></td>
                     </tr>
                 ))}
             </tbody>
         </table>
     );
 }
-
 const thStyle: React.CSSProperties = {
     border: "1px solid #ddd",
     padding: "8px",
     backgroundColor: "#f2f2f2",
-    textAlign: "left"
+    textAlign: "center"
 };
 
 const tdStyle: React.CSSProperties = {
     border: "1px solid #ddd",
-    padding: "8px"
+    padding: "8px",
+    textAlign: "center"
 };
 
 const rowStyleEven: React.CSSProperties = {
